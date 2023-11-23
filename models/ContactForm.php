158d08af1df72c5053b2e57059086ea267be0c28
pogoparis <pogoparis@gmail.com>
2023-11-23 09:33:02 +0100

    email ok

--- models/ContactForm.php
+++ models/ContactForm.php
@@ -50,7 +50,7 @@ class ContactForm extends Model
     public function contact($email)
     {
         if ($this->validate()) {
-            Yii::$app->mailer->compose()
+           $result = Yii::$app->mailer->compose()
                 ->setTo($email)
                 ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
                 ->setReplyTo([$this->email => $this->name])
