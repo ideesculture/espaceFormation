58251547155d6bfe5ac21dff1f1bb560da37dacd
pogoparis <pogoparis@gmail.com>
2023-11-24 13:34:01 +0100

    traduction fr supprimer

--- messages/fr/app.php
+++ messages/fr/app.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'Are you sure you want to delete this item?' => 'Êtes-vous sûr de vouloir supprimer cet élément ?',
+    'Êtes-vous sûr(e) de vouloir supprimer cet élément ?' => 'Êtes-vous sûr(e) de vouloir supprimer cet élément ?',
 ];
--- views/centres/view.php
+++ views/centres/view.php
@@ -20,7 +20,7 @@ $this->title = $model->name;
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'confirm' => Yii::t('app', 'Êtes-vous sûr(e) de vouloir supprimer cet élément ?'),
                 'method' => 'post',
             ],
         ]) ?>
